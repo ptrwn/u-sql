1e5b594bed7a4632450057e16ccdb0a66d5ece57
ptrwn <1374688@gmail.com>
2024-04-10 22:55:38 +0300

    setup study environment in docker

---
+++ init.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS udb;
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'udb') THEN
+
+      RAISE NOTICE 'Role "colive" already exists. Skipping.';
+   ELSE
+      create user udb with encrypted password '123';
+   END IF;
+END
+$do$;
+CREATE DATABASE udb OWNER udb;
